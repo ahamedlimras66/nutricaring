f4302a7419f82bbf2a16c17ed528775163f98b0b
muthu mani <mm4.muthumani@gmail.com>
2021-12-07 11:13:36 +0530

    fdsklj

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
-# @app.before_first_request
-# def create_table():
-#     db.create_all()
+@app.before_first_request
+def create_table():
+    db.create_all()
 
 app.register_blueprint(home, url_prefix="/")
 app.register_blueprint(about, url_prefix="/about")
